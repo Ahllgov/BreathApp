4cfcebef4d8585a6d4661275cb577f442b61b94e
makhilgov <makhilgov@ozon.ru>
2022-07-28 09:47:27 +0300

    Initial Commit

---
+++ BreathApp/BreathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathAppApp.swift
+//  BreathApp
+//
+//  Created by Akhilgov Magomed Abdulmazhitovich on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BreathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
